09aa837a811e5111541a584d5dc4ab58b0ff2adc
zehra4088194-design <zehra4088194@gmail.com>
2026-07-08 08:51:29 +0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,7 @@ const nextConfig: NextConfig = {
       { protocol: 'https', hostname: '*.supabase.co' },
       { protocol: 'https', hostname: 'lh3.googleusercontent.com' },
       { protocol: 'https', hostname: 'avatars.githubusercontent.com' },
+      { protocol: 'https', hostname: 'drive.google.com' },
     ],
     formats: ['image/avif', 'image/webp'],
   },
